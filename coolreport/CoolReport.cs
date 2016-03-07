01b78d7a402d1ccabcadbf78d56b8a2f884a0b19
mikemajesty <mikemajesty2011@gmail.com>
2016-03-06 21:16:56 -0300

    Add variables

--- coolreport/CoolReport.cs
+++ coolreport/CoolReport.cs
@@ -1,14 +1,21 @@
 ﻿using Microsoft.Reporting.WebForms;
 using System.IO;
 using System.Diagnostics;
-
-
-
+using System.Collections.Generic;
 
 namespace coolreport
 {
     public class CoolReport
-    { 
+    {
+        private ReportViewer reportViewer;
+        private List<ReportParameter> listParameter;
+        private byte[] bytePDF { get; set; }
+        Warning[] warnings;
+        private string[] streamids;
+        private string mimeType;
+        private string encoding;
+        private string extension;
+
 
     }
 }
