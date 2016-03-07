5b358816ade2584c32d2f10ee303c92fe87ed6fc
mikemajesty <mikemajesty2011@gmail.com>
2016-03-06 21:21:03 -0300

    Add enum and constructor

--- coolreport/CoolReport.cs
+++ coolreport/CoolReport.cs
@@ -15,7 +15,21 @@ namespace coolreport
         private string mimeType;
         private string encoding;
         private string extension;
+        private ReportType reporType;
+        private Type type;
+        public CoolReport(ReportType reporType, Type type)
+        {
+            this.reporType = reporType;
+            this.type = type;
+        }
 
-
+    }
+    public enum ReportType
+    {
+        Word,PDF,Excel
+    }
+    public enum Type
+    {
+        WithParameters,WithoutParameters
     }
 }
