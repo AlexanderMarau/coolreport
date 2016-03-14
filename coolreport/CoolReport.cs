5ae3aea6e5e45c275da6e37dfc5bbea51c7aaa97
mikemajesty <mikemajesty2011@gmail.com>
2016-03-14 00:13:11 -0300

    the first report - OpenReportWithoutParameters

--- coolreport/CoolReport.cs
+++ coolreport/CoolReport.cs
@@ -3,6 +3,9 @@ using System.IO;
 using System.Diagnostics;
 using System.Collections.Generic;
 using System.Collections;
+using System;
+using System.Security;
+using System.Security.Permissions;
 
 namespace coolreport
 {
@@ -16,15 +19,42 @@ namespace coolreport
         private string mimeType;
         private string encoding;
         private string extension;
-        private ReportType reporType;
-        private string reportNameWithExtension;
         private string dataSourceName;
-        public CoolReport(ReportType reporType, string reportNameWithExtension, string dataSourceName)
+        private static string extensionName;
+       
+        public CoolReport(ReportType reportType, string reportNameWithExtension, string dataSourceName, ProcessingMode processingMode = ProcessingMode.Local)
         {
-            this.reporType = reporType;
-            this.reportNameWithExtension = reportNameWithExtension;
+            reportViewer = new ReportViewer();
+            reportViewer.ProcessingMode = processingMode;
             this.dataSourceName = dataSourceName;
+            extensionName = reportType == ReportType.Excel ? ".Excel" : reportType == ReportType.Word ? ".Word" : ".pdf";
+            /*reportViewer.LocalReport.ReportEmbeddedResource ="Mike.Utilities.Desktop.teste.rdlc";  
+            se for rodar no mesmo assembly
+            */
+            reportViewer.LocalReport.ReportPath = reportNameWithExtension;
+            reportViewer.LocalReport.SetBasePermissionsForSandboxAppDomain(new PermissionSet(PermissionState.Unrestricted));
         }
+        public void OpenReportWithoutParameters(IEnumerable _query, string reportName = "Report")
+        {
+
+                var list = new ReportDataSource(name: dataSourceName, dataSourceValue: _query);
+                reportViewer.LocalReport.DataSources.Add(list);
+                byte[] bytePDF = reportViewer.LocalReport.Render(extensionName = extensionName.Remove(0,1).ToUpper(), null, out mimeType, out encoding, out extension, out streamids, out warnings);
+                FileStream fileStream = null;
+                string fileName = null;
+                fileName = GeneratePath(reportName);
+                fileStream = new FileStream(fileName, FileMode.Create);
+                fileStream.Write(bytePDF, 0, bytePDF.Length);
+                fileStream.Close();
+                Process.Start(fileName);
+           
+        }
+
+        private static string GeneratePath(string reportName)
+        {            
+            return $"{Path.GetTempPath()} {reportName} - {DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss")}.{extensionName}"; 
+        }
+      
     }
 
 }
@@ -34,5 +64,6 @@ public enum ReportType
 {
     Word, PDF, Excel
 }
-   
-}
+
+
+
